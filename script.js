b23d284fb2cfb676c985398c2ea2cd27fed6dbac
janasyarivers14-png <janasyarivers14@gmail.com>
2026-07-08 21:58:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -107,3 +107,83 @@ function addBrownies() {
         addToCart("Brownie", 1.50);
     }
 }
+function addMultiple(name, price, qty) {
+  for (let i = 0; i < qty; i++) {
+    addToCart(name, price);
+  }
+}
+
+function addWing5() {
+  addMultiple(
+    `5 Piece Wing Meal - ${wing5Flavor.value}, ${wing5Type.value}, ${wing5Drink.value}`,
+    10,
+    Number(wing5Qty.value)
+  );
+}
+
+function addWing10() {
+  addMultiple(
+    `10 Piece Wing Meal - ${wing10Flavor.value}, ${wing10Type.value}, ${wing10Drink.value}`,
+    15,
+    Number(wing10Qty.value)
+  );
+}
+
+function addCombo() {
+  addMultiple(
+    `Wing + Rasta Pasta Combo - ${comboFlavor.value}, ${comboDrink.value}`,
+    18,
+    Number(comboQty.value)
+  );
+}
+
+function addCake() {
+  const option = cakeSize.options[cakeSize.selectedIndex];
+  addMultiple(
+    `${option.value} ${cakeFlavor.value} Mini Cake`,
+    Number(option.dataset.price),
+    Number(cakeQty.value)
+  );
+}
+
+function addCookies() {
+  addMultiple(
+    `${cookieFlavor.value} Cookie`,
+    2,
+    Number(cookieQty.value)
+  );
+}
+
+function addStrawberries() {
+  const option = strawberrySize.options[strawberrySize.selectedIndex];
+  addMultiple(
+    `${option.value} Chocolate Covered Strawberries`,
+    Number(option.dataset.price),
+    Number(strawberryQty.value)
+  );
+}
+
+function addPretzels() {
+  addMultiple(
+    "Chocolate Covered Pretzel",
+    1,
+    Number(pretzelQty.value)
+  );
+}
+
+function addBrownies() {
+  addMultiple(
+    "Brownie",
+    1.50,
+    Number(brownieQty.value)
+  );
+}
+
+function addBananaPudding() {
+  const option = bananaSize.options[bananaSize.selectedIndex];
+  addMultiple(
+    `${option.value} Banana Pudding Pan`,
+    Number(option.dataset.price),
+    Number(bananaQty.value)
+  );
+}
